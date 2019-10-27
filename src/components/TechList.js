b80c0d86e3d38cea63c9d308047f486629486c7a
Luis Geniole <luis.geniole@gmail.com>
2019-10-27 14:48:20 -0300

    add component form&button&setState

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -4,20 +4,48 @@ import profile from '../assets/myAvatar.png';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: ['Node.js', 'ReactJS', 'React Native'],
+  };
+
+  handleInputChange = e => {
+    console.log(e.target.value);
+
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state.newTech);
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
   };
 
   render() {
     console.log(this.state);
-    return [
-      <h1>Howdy Partner!!!🤠</h1>,
-      <img src={profile} />,
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>,
-    ];
+
+    return (
+      <>
+        <h1>Howdy Partner!!!🤠</h1>,
+        <img src={profile} />,<h2>{this.state.newTech}</h2>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <li key={tech}>{tech}</li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+          />
+          <button type="submit">Send!</button>
+        </form>
+      </>
+    );
   }
 }
 
